cf6a92c8d0fc7ec9e778df72baa14397df7cb84d
Yimin <864649213@qq.com>
2019-11-23 18:13:57 +0100

    player basic movement

---
+++ Assets/Scripts/cameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScript : MonoBehaviour
+{
+    public Vector3 offset = new Vector3(0, 1, -5);
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerPos = GameObject.Find("Player").transform.position;
+        transform.position = playerPos + offset;
+
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float forwardForce = 1000;
+    public float jumpForce = 30;
+    public float sideForce = 500;
+
+    Rigidbody rb;
+    bool jumping = false;
+    float timeBetweenMovements = 0.5f;
+    float lastMovementTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (!jumping && Time.time - lastMovementTime >= timeBetweenMovements)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                rb.AddForce(0, jumpForce * Time.deltaTime, 0);
+                //jumping = true;
+                lastMovementTime = Time.time;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+                lastMovementTime = Time.time;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+                lastMovementTime = Time.time;
+            }
+        }
+        
+    }
+}
