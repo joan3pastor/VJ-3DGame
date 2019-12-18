077b810b4105834f3eee18c51166aac7a538e1e0
joan3pastor <joan.pastor@hotmail.com>
2019-12-18 03:03:45 +0100

    Menus

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+
+    public int IndexScene;
+
+    public void ChangeScene() {
+        Application.LoadLevel(IndexScene);
+    }
+
+}
---
+++ Assets/Scripts/manageLvLMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class manageLvLMenu : MonoBehaviour
+     , IPointerClickHandler
+     , IPointerEnterHandler
+     , IPointerExitHandler
+{
+
+    public int levelSceneIndex;
+
+
+    void Start()
+    {
+        Debug.Log("I WORK");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+        Application.LoadLevel(levelSceneIndex);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Over");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Out");
+    }
+
+}
